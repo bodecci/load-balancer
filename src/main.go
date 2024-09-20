a80f30cf2f3b5b64069ade45a207e2e8533c4b6f
Bode <bode.falade@outlook.com>
2024-09-19 22:44:43 -0500

    finished the laod balancer funcs and methods.

--- src/main.go
+++ src/main.go
@@ -49,3 +49,64 @@ func handlerErr(err error) {
 		os.Exit(1)
 	}
 }
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
+}
+
+func (s *simpleServer) Address() string {
+	return s.addr
+}
+
+func (s *simpleServer) IsAlive() bool {
+	return true
+}
+
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
+}
+
+// getNextServerAddr returns the address of the next available server to send a
+// request to, using round-robin algorithm
+func (lb *LoadBalancer) getNextAvailable() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+
+	return server
+}
+
+func (lb LoadBalancer) serveProxy(rw http.ResponseWriter, req *http.Request) {
+	// get next server avaible.
+	targetServer := lb.getNextAvailable()
+
+	fmt.Printf("forwarding request to address %q\n", targetServer.Address())
+
+	targetServer.Serve(rw, req)
+}
+
+func main() {
+	// setup servers
+	servers := []Server{
+		newSimpleServer("https://www.facebook.com"),
+		newSimpleServer("http://www.bing.com"),
+		newSimpleServer("http://www.duckduckgo.com"),
+	}
+
+	lb := NewLoadBalancer("8000", servers)
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serveProxy(rw, req)
+	}
+
+	http.HandleFunc("/", handleRedirect)
+
+	fmt.Printf("serving requests at locahost:%s '\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
+}
